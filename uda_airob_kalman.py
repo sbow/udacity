27441aa8253cebdc75c34cccd37731c40d4157dc
Shaun Bowman <shaun_bowman@hotmail.com>
2017-10-03 17:46:59 -0400

    notes edit

--- uda_airob_kalman.py
+++ uda_airob_kalman.py
@@ -85,4 +85,30 @@ print [mu, sig]
 # so if you know something about x, it implies something about y
 # Application: for 1D position measurements, invent 2nd dimetion for velocity
 
+# Excersice 25
+# Example: if you know a physical relationship like position equals prior
+# position + velocity:  x' = x + x_dot*delta_t
+# you can get the predicted position at the NEXT unobserved timestep by
+# multiplying the mu/sigma of your current measurement position estimate by the guassian
+# distrubution relating x_dot to x' ie for x_1, and relationship x' = x + x_dot
+# the prior distrobution relating x' would be a slanted line centered through
+# the following points: [x, x_dot] = [0,-1],[1,0],[2,1],[3,2] ect
+# This is an example of using a measureable variable (x) to infer a non
+# measureable variable (x_dot)
+# Kalman Filter variables(ie: x, x_dot, their sigmas / mu's) are called "Kalman
+# States"; and they are divided into 2 types "Observable & Hidden"
+# Kalman Filter is very efficiant for problems when you need to estimate a
+# hidden variable using observable variables.
+# To design a kalman filter you need 2 things typically:
+# State Transision Function: defines relationship between x', x & x_dot (ie: x'
+# = x + x_dot; this is a matrix (typ square) (x',xdot') = (StateTransMtrx)*
+# (x;xdot)
+# Measurement Transision Function: defines relationships between "Z" and
+# x,xdot, typically a column vector, Z = (MeasTransFunc)*(x;xdot) 
+#
+# example: new position = old position + vel&deltaT; new velocity = old
+# velocity | state transision matrix = ( 1, 1; 0, 1 ) or x',xdot' = (1, 1;
+# 0,1)*(x; xdot)
+# measurement Z = old postion only (not velocity, ie: a camera image), the
+# measuremente transision function equals (1, 0) or z = (1,0)*(x;xdot)
 
